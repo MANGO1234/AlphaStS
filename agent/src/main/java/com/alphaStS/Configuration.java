0dd4b308dbd4d505dc7a52885bde526d29125998
MANGO1234 <814583+MANGO1234@users.noreply.github.com>
2023-10-02 02:55:17 -0700

    change to gpu

--- agent/src/main/java/com/alphaStS/Configuration.java
+++ agent/src/main/java/com/alphaStS/Configuration.java
@@ -111,5 +111,5 @@ public class Configuration {
     public static boolean USE_NEW_ACTION_SELECTION = false;
 
     public static final String ONNX_LIB_PATH = "F:/git/lib";
-    public static final boolean ONNX_USE_CUDA_FOR_INFERENCE = false;
+    public static final boolean ONNX_USE_CUDA_FOR_INFERENCE = true;
 }
