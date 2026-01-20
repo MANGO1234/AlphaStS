c6828144fbf8174f12f4c30fac74e918b3e0db89
MANGO1234 <814583+MANGO1234@users.noreply.github.com>
2026-01-19 20:18:16 -0800

    Fix interactive.py classpath to use default Maven repository for linux
    Use ~/.m2/repository as default instead of requiring M2_HOME env var,
    and update dependency versions to match current pom.xml.

--- interactive.py
+++ interactive.py
@@ -7,8 +7,20 @@ sep = ':'
 if platform.system() == 'Windows':
     sep = ';'
 
+# Use M2_REPO env var or default to ~/.m2/repository
+M2_REPO = os.getenv("M2_REPO", os.path.expanduser("~/.m2/repository"))
 
-CLASS_PATH = f'./target/classes{sep}{os.getenv("M2_HOME")}/repository/com/microsoft/onnxruntime/onnxruntime/1.10.0/onnxruntime-1.10.0.jar{sep}./src/resources/mallet.jar{sep}./src/resources/mallet-deps.jar{sep}{os.getenv("M2_HOME")}/repository/org/jdom/jdom/1.1/jdom-1.1.jar{sep}{os.getenv("M2_HOME")}/repository/com/fasterxml/jackson/core/jackson-databind/2.12.4/jackson-databind-2.12.4.jar{sep}{os.getenv("M2_HOME")}/repository/com/fasterxml/jackson/core/jackson-annotations/2.12.4/jackson-annotations-2.12.4.jar{sep}{os.getenv("M2_HOME")}/repository/com/fasterxml/jackson/core/jackson-core/2.12.4/jackson-core-2.12.4.jar{sep}{os.getenv("M2_HOME")}/repository/org/apache/commons/commons-compress/1.21/commons-compress-1.21.jar{sep}{os.getenv("M2_HOME")}/repository/org/apache/commons/commons-math3/3.6.1/commons-math3-3.6.1.jar{sep}{os.getenv("M2_HOME")}/repository/one/util/streamex/0.8.3/streamex-0.8.3.jar'
+CLASS_PATH = sep.join([
+    './target/classes',
+    f'{M2_REPO}/com/microsoft/onnxruntime/onnxruntime_gpu/1.10.0/onnxruntime_gpu-1.10.0.jar',
+    f'{M2_REPO}/org/jdom/jdom/1.1/jdom-1.1.jar',
+    f'{M2_REPO}/com/fasterxml/jackson/core/jackson-databind/2.18.4/jackson-databind-2.18.4.jar',
+    f'{M2_REPO}/com/fasterxml/jackson/core/jackson-annotations/2.18.4/jackson-annotations-2.18.4.jar',
+    f'{M2_REPO}/com/fasterxml/jackson/core/jackson-core/2.18.4/jackson-core-2.18.4.jar',
+    f'{M2_REPO}/org/apache/commons/commons-compress/1.26.0/commons-compress-1.26.0.jar',
+    f'{M2_REPO}/org/apache/commons/commons-math3/3.6.1/commons-math3-3.6.1.jar',
+    f'{M2_REPO}/one/util/streamex/0.8.3/streamex-0.8.3.jar',
+])
 
 os.chdir('agent')
 agent_args = ['java', '--add-opens', 'java.base/java.util=ALL-UNNAMED', '-classpath', CLASS_PATH, 'com.alphaStS.Main', '-i']
