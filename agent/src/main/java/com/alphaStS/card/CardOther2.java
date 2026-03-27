f6623a21625ef65b3dbba627bfc7f423400eff12
MANGO1234 <814583+MANGO1234@users.noreply.github.com>
2026-03-27 01:56:22 -0700

    Implement STS2 Status and Curse cards in CardOther2

--- agent/src/main/java/com/alphaStS/card/CardOther2.java
+++ agent/src/main/java/com/alphaStS/card/CardOther2.java
@@ -1,145 +1,185 @@
 package com.alphaStS.card;
 
+import com.alphaStS.*;
+import com.alphaStS.gameAction.GameActionCtx;
+
 public class CardOther2 {
     // **************************************************************************************************
     // ********************************************* Status *********************************************
     // **************************************************************************************************
 
-    // TODO: Beckon (Status) - 1 energy, Status
-    //   Effect: At the end of your turn, if this is in your Hand,  lose 6 HP.
-    //   No upgrade.
+    public static class Beckon extends Card {
+        public Beckon() {
+            super("Beckon", Card.STATUS, 1, Card.COMMON);
+            alwaysDiscard = true;
+        }
 
-    // TODO: Burn (Status) - Unplayable energy, Status
-    //   Effect: Unplayable. At the end of your turn, if this is in your Hand, take 2 damage.
-    //   No upgrade.
+        @Override public void onDiscardEndOfTurn(GameState state, int numCardsInHand) {
+            state.doNonAttackDamageToPlayer(6, false, this);
+        }
+    }
 
-    // TODO: Dazed (Status) - Unplayable energy, Status
-    //   Effect: Unplayable. Ethereal.
-    //   No upgrade.
+    public static class Burn extends CardOther.Burn {}
 
-    // TODO: Debris (Status) - 1 energy, Status
-    //   Effect: Exhaust.
-    //   No upgrade.
+    public static class Dazed extends CardOther.Dazed {}
 
-    // TODO: Disintegration (Status) - Unplayable energy, Status
-    //   Effect: At the end of your turn, take 6 damage.
-    //   No upgrade.
+    public static class Debris extends Card {
+        public Debris() {
+            super("Debris", Card.STATUS, 1, Card.COMMON);
+            exhaustWhenPlayed = true;
+        }
+    }
+
+    public static class Disintegration extends Card {
+        public Disintegration() {
+            super("Disintegration", Card.STATUS, -1, Card.COMMON);
+            alwaysDiscard = true;
+        }
+
+        @Override public void onDiscardEndOfTurn(GameState state, int numCardsInHand) {
+            state.doNonAttackDamageToPlayer(6, true, this);
+        }
+    }
 
     // TODO: Frantic Escape (Status) - 1 energy, Status
     //   Effect: Get farther away. Increase Sandpit by 1. Increase the cost of this card by 1.
     //   No upgrade.
 
-    // TODO: Infection (Status) - Unplayable energy, Status
-    //   Effect: Unplayable. At the end of your turn, if this is in your Hand, take 3 damage.
-    //   No upgrade.
+    public static class Infection extends Card {
+        public Infection() {
+            super("Infection", Card.STATUS, -1, Card.COMMON);
+            alwaysDiscard = true;
+        }
+
+        @Override public void onDiscardEndOfTurn(GameState state, int numCardsInHand) {
+            state.doNonAttackDamageToPlayer(3, true, this);
+        }
+    }
 
     // TODO: Mind Rot (Status) - Unplayable energy, Status
     //   Effect: Draw 1 fewer card each turn.
     //   No upgrade.
 
-    // TODO: Slimed (Status) - 1 energy, Status
-    //   Effect: Draw 1 card. Exhaust.
-    //   No upgrade.
+    // Slimed draws 1 card when played, unlike STS1 which only exhausts.
+    public static class Slimed extends Card {
+        public Slimed() {
+            super("Slimed", Card.STATUS, 1, Card.COMMON);
+            exhaustWhenPlayed = true;
+        }
+
+        @Override public GameActionCtx play(GameState state, int idx, int energyUsed) {
+            state.draw(1);
+            return GameActionCtx.PLAY_CARD;
+        }
+    }
 
     // TODO: Sloth (Status) - Unplayable energy, Status
     //   Effect: You cannot play more than 3 cards each turn.
     //   No upgrade.
 
-    // TODO: Soot (Status) - Unplayable energy, Status
-    //   Effect: Unplayable.
-    //   No upgrade.
+    public static class Soot extends Card {
+        public Soot() {
+            super("Soot", Card.STATUS, -1, Card.COMMON);
+        }
+    }
 
-    // TODO: Toxic (Status) - 1 energy, Status
-    //   Effect: At the end of your turn, if this is in your Hand, take 5 damage. Exhaust.
-    //   No upgrade.
+    public static class Toxic extends Card {
+        public Toxic() {
+            super("Toxic", Card.STATUS, 1, Card.COMMON);
+            exhaustWhenPlayed = true;
+            alwaysDiscard = true;
+        }
 
-    // TODO: Void (Status) - Unplayable energy, Status
-    //   Effect: Unplayable. Ethereal. Whenever you draw this card, lose energy.
-    //   No upgrade.
+        @Override public void onDiscardEndOfTurn(GameState state, int numCardsInHand) {
+            state.doNonAttackDamageToPlayer(5, true, this);
+        }
+    }
+
+    public static class Void extends CardOther.Void {}
 
     // TODO: Waste Away (Status) - Unplayable energy, Status
     //   Effect: Gain 1 less energy per turn.
     //   No upgrade.
 
-    // TODO: Wound (Status) - Unplayable energy, Status
-    //   Effect: Unplayable.
-    //   No upgrade.
+    public static class Wound extends CardOther.Wound {}
 
     // **************************************************************************************************
     // ********************************************* Curse  *********************************************
     // **************************************************************************************************
 
-    // TODO: Ascender's Bane (Curse) - Unplayable energy, Curse
-    //   Effect: Unplayable. Ethereal. Eternal.
-    //   No upgrade.
+    public static class AscendersBane extends CardOther.AscendersBane {}
 
-    // TODO: Bad Luck (Curse) - Unplayable energy, Curse
-    //   Effect: Unplayable. At the end of your turn, if this is in your Hand, lose 13 HP. Eternal.
-    //   No upgrade.
+    public static class BadLuck extends Card {
+        public BadLuck() {
+            super("Bad Luck", Card.CURSE, -1, Card.COMMON);
+            exhaustWhenPlayed = true;
+            alwaysDiscard = true;
+        }
 
-    // TODO: Clumsy (Curse) - Unplayable energy, Curse
-    //   Effect: Unplayable. Ethereal.
-    //   No upgrade.
+        @Override public void onDiscardEndOfTurn(GameState state, int numCardsInHand) {
+            state.doNonAttackDamageToPlayer(13, false, this);
+        }
+    }
 
-    // TODO: Curse of the Bell (Curse) - Unplayable energy, Curse
-    //   Effect: Unplayable. Eternal.
-    //   No upgrade.
+    public static class Clumsy extends CardOther.Clumsy {}
+
+    public static class CurseOfTheBell extends CardOther.CurseOfTheBell {}
+
+    public static class Decay extends CardOther.Decay {}
 
     // TODO: Debt (Curse) - Unplayable energy, Curse
     //   Effect: Unplayable. At the end of your turn, if this is in your Hand, lose 10 Gold.
     //   No upgrade.
 
-    // TODO: Decay (Curse) - Unplayable energy, Curse
-    //   Effect: Unplayable. At the end of your turn, if this is in your Hand, take 2 damage.
-    //   No upgrade.
-
-    // TODO: Doubt (Curse) - Unplayable energy, Curse
-    //   Effect: Unplayable. At the end of your turn, if this is in your Hand, gain 1 Weak.
-    //   No upgrade.
+    public static class Doubt extends CardOther.Doubt {}
 
     // TODO: Enthralled (Curse) - 2 energy, Curse
     //   Effect: If this is in your Hand, it must be played before other cards. Eternal.
     //   No upgrade.
 
-    // TODO: Folly (Curse) - Unplayable energy, Curse
-    //   Effect: Unplayable. Innate. Eternal.
-    //   No upgrade.
+    public static class Folly extends Card {
+        public Folly() {
+            super("Folly", Card.CURSE, -1, Card.COMMON);
+            exhaustWhenPlayed = true;
+            innate = true;
+        }
+    }
 
-    // TODO: Greed (Curse) - Unplayable energy, Curse
-    //   Effect: Unplayable. Eternal.
-    //   No upgrade.
+    public static class Greed extends Card {
+        public Greed() {
+            super("Greed", Card.CURSE, -1, Card.COMMON);
+            exhaustWhenPlayed = true;
+        }
+    }
 
     // TODO: Guilty (Curse) - Unplayable energy, Curse
     //   Effect: Unplayable. Removed from your Deck after 5 combats.
     //   No upgrade.
 
-    // TODO: Injury (Curse) - Unplayable energy, Curse
-    //   Effect: Unplayable.
-    //   No upgrade.
+    public static class Injury extends CardOther.Injury {}
 
-    // TODO: Normality (Curse) - Unplayable energy, Curse
-    //   Effect: Unplayable. You cannot play more than 3 cards this turn.
-    //   No upgrade.
+    public static class Normality extends CardOther.Normality {}
 
-    // TODO: Poor Sleep (Curse) - Unplayable energy, Curse
-    //   Effect: Unplayable. Retain.
-    //   No upgrade.
+    public static class PoorSleep extends Card {
+        public PoorSleep() {
+            super("Poor Sleep", Card.CURSE, -1, Card.COMMON);
+            exhaustWhenPlayed = true;
+            retain = true;
+        }
+    }
 
-    // TODO: Regret (Curse) - Unplayable energy, Curse
-    //   Effect: Unplayable. At the end of your turn, if this is in your Hand, lose 1 HP for each card in your Hand.
-    //   No upgrade.
+    public static class Regret extends CardOther.Regret {}
 
-    // TODO: Shame (Curse) - Unplayable energy, Curse
-    //   Effect: Unplayable. At the end of your turn, if this is in your Hand, gain 1 Frail.
-    //   No upgrade.
+    public static class Shame extends CardOther.Shame {}
 
-    // TODO: Spore Mind (Curse) - 1 energy, Curse
-    //   Effect: Exhaust.
-    //   No upgrade.
+    public static class SporesMind extends Card {
+        public SporesMind() {
+            super("Spore Mind", Card.CURSE, 1, Card.COMMON);
+            exhaustWhenPlayed = true;
+        }
+    }
 
-    // TODO: Writhe (Curse) - Unplayable energy, Curse
-    //   Effect: Unplayable. Innate.
-    //   No upgrade.
+    public static class Writhe extends CardOther.Writhe {}
 
     // **************************************************************************************************
     // ********************************************* Quest  *********************************************
